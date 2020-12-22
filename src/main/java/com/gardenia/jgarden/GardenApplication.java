4d5e458e160b14b5dc66cadc5d18c9736b1e128c
zuozhiwen <zzwqqmail@qq.com>
2020-12-22 10:16:34 +0800

    项目重新起航

--- src/main/java/com/gardenia/jgarden/GardenApplication.java
+++ src/main/java/com/gardenia/jgarden/GardenApplication.java
@@ -1,11 +1,9 @@
-package com.fvotlan.garden;
+package com.gardenia.jgarden;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class GardenApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/gardenia/jgarden/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.gardenia.jgarden.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+/**
+ * @author zhiwen.zuo
+ * @date 2019-05-27
+ **/
+@RestController
+@RequestMapping("hello")
+@AllArgsConstructor
+public class HelloController {
+
+    @GetMapping
+    public Object world() {
+        return new HashMap<String, Object>() {{
+            put("code", 0);
+            put("msg", "success");
+        }};
+    }
+}
